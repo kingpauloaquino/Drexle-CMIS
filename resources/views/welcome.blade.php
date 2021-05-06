793fe62fe7d0780b8d14df409da526d63326d619
kingpauloaquino <kingpauloaquino@gmail.com>
2021-05-06 20:23:19 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
 
     <!--====== Title ======-->
-    <title>Barangya East Bajac-bajac</title>
+    <title>Barangay East Bajac-bajac</title>
 
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
